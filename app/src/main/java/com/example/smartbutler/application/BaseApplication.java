7a6147d3b07603a3ae73426c87368317026c0625
蔡万鸿 <caiwanhong@koolearn.com>
2019-01-17 15:33:30 +0800

    登录注册界面开发

--- app/src/main/java/com/example/smartbutler/application/BaseApplication.java
+++ app/src/main/java/com/example/smartbutler/application/BaseApplication.java
@@ -5,12 +5,17 @@ import android.app.Application;
 import com.example.smartbutler.utils.StaticClass;
 import com.tencent.bugly.crashreport.CrashReport;
 
+import cn.bmob.v3.Bmob;
+
 public class BaseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        CrashReport.initCrashReport(getApplicationContext(), StaticClass.BUGLY_APP_ID, false);
+        //初始化Bugly
+        CrashReport.initCrashReport(getApplicationContext(), StaticClass.BUGLY_APP_ID, true);
+        //初始化Bmob
+        Bmob.initialize(this, StaticClass.BMOB_APP_ID);
     }
 }
